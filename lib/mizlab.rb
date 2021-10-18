5d2f45812dc2a73a2175e41ed25eed3ee6d7d335
mochi-MizLab <mochice.mls.ntl@gmail.com>
2021-10-18 23:28:01 +0900

    :+1: Add bresenham method

--- lib/mizlab.rb
+++ lib/mizlab.rb
@@ -4,5 +4,37 @@ require_relative "mizlab/version"
 
 module Mizlab
   class Error < StandardError; end
-  # Your code goes here...
+
+  def local_patterns(x_coordinates, y_coordinates)
+  end
+
+  def bresenham(x0, y0, x1, y1)
+    if !x0.is_a?(Integer) || !y0.is_a?(Integer) || !x1.is_a?(Integer) || !y1.is_a?(Integer)
+      raise TypeError, 'All of arguments must be Integer'
+    end
+    dx = (x1 - x0).abs
+    dy = (y1 - y0).abs
+    sx = x0 < x1 ? 1 : -1
+    sy = y0 < y1 ? 1 : -1
+    err = dx - dy
+    lines = []
+    while true
+      lines.append([x0, y0])
+      if (x0 == x1 && y0 == y1)
+        break
+      end
+      e2 = 2 * err
+      if e2 > -dy
+        err = err - dy
+        x0 = x0 + sx
+      end
+      if e2 < dx
+        err = err + dx
+        y0 = y0 + sy
+      end
+    end
+    return lines
+  end
+
+  module_function :bresenham
 end
